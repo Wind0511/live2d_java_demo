bc3cdbe2cfbe8f785e411ecebcdd2077debb126e
Wind0511 <42757389+Wind0511@users.noreply.github.com>
2019-11-27 12:00:25 +0800

    Update ModelList.java

--- src/main/java/com/wind/live2dDemo/util/ModelList.java
+++ src/main/java/com/wind/live2dDemo/util/ModelList.java
@@ -7,27 +7,12 @@ import java.io.IOException;
 
 public class ModelList {
 
-	/**
-	 * 
-	 * @Description:获取模型列表
-	 * @return
-	 * @author: chenhaoyu
-	 * @throws IOException
-	 * @time:Mar 23, 2019 10:36:41 AM
-	 */
+	
 	public static JSONObject getList() throws IOException {
 		return FileUtils.readJsonFromClassPath("model_list.json", JSONObject.class);
 	}
 
-	/**
-	 * 
-	 * @Description:获取模组名称
-	 * @param id
-	 * @return
-	 * @throws IOException
-	 * @author: chenhaoyu
-	 * @time:Mar 23, 2019 12:04:30 PM
-	 */
+	
 	public static Object idToName(int id) throws IOException {
 		JSONObject modelListJson = getList();
 		JSONArray models = modelListJson.getJSONArray("models");
