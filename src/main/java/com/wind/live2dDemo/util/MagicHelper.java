596b6f033bb2663f1ae84582570adf3fd72e366e
Wind0511 <42757389+Wind0511@users.noreply.github.com>
2019-11-27 11:59:59 +0800

    Update MagicHelper.java

--- src/main/java/com/wind/live2dDemo/util/MagicHelper.java
+++ src/main/java/com/wind/live2dDemo/util/MagicHelper.java
@@ -7,14 +7,6 @@ public class MagicHelper {
 	public static final String ID = "id";
 	public static final String TID = "tid";
 
-	/**
-	 * 
-	 * @Description: 统一处理字符型 x-y 类型id，如果出现异常则用默认id
-	 * @param origin
-	 * @return
-	 * @author: chenhaoyu
-	 * @time:Mar 25, 2019 10:09:52 AM
-	 */
 	public static Map<String, Integer> getIdMap(String origin) {
 		Map<String, Integer> result = new HashMap<String, Integer>();
 		result.put(ID, 1);
